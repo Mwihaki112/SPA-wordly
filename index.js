6de12a356b23c39bed75cc01c2a9fdb9d78a2031
Agnes Mwihaki <moringa@DESKTOP-89EPAHO.localdomain>
2026-01-19 13:38:10 +0300

    lastt commit

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 let searchInput = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 
+//Fetch word data from API
 const getData = async (searchValue) => {
    try{ 
     let data= await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`);
+
+    //Check if word exists
     if(!data.ok){
         throw new Error("Word not found");
     }
     let jsonData= await data.json();
 
+    //Clear previous results
 document.querySelector(".text").innerHTML="";
+
+//Create result box
 let div = document.createElement("div");
     div.classList.add("detail");
+    //display word details
     div.innerHTML=`
        <h2>Word: <span>${jsonData[0].word}</span></h2>
        <p>${jsonData[0].meanings[0].partOfSpeech}</p>
@@ -21,14 +28,19 @@ let div = document.createElement("div");
        <a href = ${jsonData[0].sourceUrls[0]} target="_blank"> Read More </a>
 `;
 
+//show result
 document.querySelector(".text").appendChild(div);
    } catch(error){
+//show error message    
 document.querySelector(".text").innerHTML ="Sorry, word not found. ";    
    }
 }; 
 
+//Button click event
 searchBtn.addEventListener("click", function(){
     let searchValue = searchInput.value;
+    
+    //Check input
     if(searchValue === ""){
         alert("First Enter Word")
     }else{
